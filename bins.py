44bd4d68a5d294a9131d8355b7788d992cd19f6e
wesleyOnLucy <wesley@heungs.com>
2015-08-10 13:01:28 -0400

    Removed a bunch of unnecessary stuff.

--- bins.py
+++ bins.py
@@ -1,20 +1,16 @@
+# Python 3.4.3
+from itertools import *;
+
 # Initialize
 elements = [0, 1, 2, 3, 4, 5, 6];
 n = len(elements); # Number of elements
 m = 3; # Number of bins
-bins = [[] for x in range(m)];
 
 # Precalculate this
-elementsPerBinCeil = n / m;
-elementsPerBinFloor = n / m - 1;
-# This is the bin number above which we have to use elementsPerBinFloor
 cutoffNum = n % m;
-i = 0; # This is which bin to assign the element to
 # Assign all elements to a bin
-for element in elements:
-    bins[i].append(element);
-    # Move to next bin
-    if (i < cutoffNum and len(bins[i]) > elementsPerBinCeil):
-        i += 1;
-    elif (i >= cutoffNum and len(bins[i]) > elementsPerBinFloor):
-        i += 1;
+indexes = [n // m + 1] * cutoffNum + [n // m] * (m - cutoffNum);
+indexes = list(accumulate(indexes));
+indexes = zip([0] + indexes[:-1], indexes);
+bins = [[e for e in elements[s:e]] for (s, e) in indexes];
+print(bins);
